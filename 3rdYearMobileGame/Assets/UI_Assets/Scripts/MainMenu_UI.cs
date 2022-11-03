8a85e9ae36d7eec8dd390acdce7bf674cbc6c479
Joshua Mootoo <joshuamoo2@hotmail.com>
2022-11-03 17:47:54 +0000

    Pause Menu added
    Added pause menu and updated ui elements

--- 3rdYearMobileGame/Assets/UI_Assets/Scripts/MainMenu_UI.cs
+++ 3rdYearMobileGame/Assets/UI_Assets/Scripts/MainMenu_UI.cs
@@ -8,17 +8,7 @@ public class MainMenu_UI : MonoBehaviour
 {
     [Header("Menu Buttons")]
     [SerializeField]
-    private Button startButton;
-    [SerializeField]
-    private Button settingsButton;
-    [SerializeField]
     private Button quitButton;
-
-    public void OnStartClicked ()
-    {
-        //Debug.Log("Start Button Clicked");
-        SceneManager.LoadScene(1);
-    }
 
     public void OnQuitClicked ()
     {
---
+++ 3rdYearMobileGame/Assets/UI_Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeButton()
+    {
+        Time.timeScale = 1;
+    }
+}
