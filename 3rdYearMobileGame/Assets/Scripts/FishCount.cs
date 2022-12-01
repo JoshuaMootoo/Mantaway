313e8af9ae9ad8e27c52281156e267ddd46895a5
JimmyRammelkamp <jimi334455@gmail.com>
2022-12-01 16:59:07 +0000

    Fish Meter

--- 3rdYearMobileGame/Assets/Scripts/FishCount.cs
+++ 3rdYearMobileGame/Assets/Scripts/FishCount.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class FishCount : MonoBehaviour
 {
     public TextMeshProUGUI fishCount;
+    public Slider slider;
 
     public int collectedfish;
     public int maxFish;
+
+    float sliderPercent;
 
     // Start is called before the first frame update
     void Start()
@@ -22,17 +26,15 @@ public class FishCount : MonoBehaviour
         {
             maxFish += crate.heldFish;
         }
+        slider = GetComponent<Slider>();
+        slider.maxValue = maxFish;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void SetFishCount(int foundFish)
     {
         collectedfish = foundFish;
-        fishCount.SetText("Fish " + collectedfish + "/" + maxFish);
+        fishCount.SetText(collectedfish + "/" + maxFish);
+        slider.value = foundFish;
     }
 }
