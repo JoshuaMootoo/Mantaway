9f6872495f0a37d3b9c2ac83d281fc11b5e4207c
Joshua Mootoo <joshuamoo2@hotmail.com>
2022-12-11 18:21:26 +0000

    Added Distance limit to the OilController and resized oil prefab

--- 3rdYearMobileGame/Assets/Scripts/OilController.cs
+++ 3rdYearMobileGame/Assets/Scripts/OilController.cs
@@ -8,6 +8,10 @@ public class OilController : MonoBehaviour
     float oilStarterSpeed;
     public float oilAcceleration = 0.2f;
 
+    public float speedMultiplyer = 1.5f;
+    public float startDistanceFromPlayer;
+    public float distanceFromPlayer;
+
     PlayerController player;
     
     // Start is called before the first frame update
@@ -16,12 +20,16 @@ public class OilController : MonoBehaviour
         player = FindObjectOfType<PlayerController>();
         oilStarterSpeed = player.playerDefaultSpeed - 1;
         oilSpeed = oilStarterSpeed;
+        startDistanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * oilSpeed * Time.deltaTime);
+        distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceFromPlayer > startDistanceFromPlayer) transform.Translate(Vector3.forward * oilSpeed * speedMultiplyer * Time.deltaTime);
+        else transform.Translate(Vector3.forward * oilSpeed * Time.deltaTime);
 
         oilSpeed += oilAcceleration * Time.deltaTime;
     }
