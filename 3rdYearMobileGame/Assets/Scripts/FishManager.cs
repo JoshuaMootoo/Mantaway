9be0f609df8f6995b2cbe0a6a5debc164a81a355
JimmyRammelkamp <jimi334455@gmail.com>
2022-12-21 19:33:09 +0000

    Tutorial 1 Decorated, Fish manager code commented

--- 3rdYearMobileGame/Assets/Scripts/FishManager.cs
+++ 3rdYearMobileGame/Assets/Scripts/FishManager.cs
@@ -8,14 +8,17 @@ public class FishManager : MonoBehaviour
 
     public Transform targetObject;
 
+    //fish actively following the player
     public List<FishController> FishList;
     public List<FishController> FishList2;
     public List<FishController> FishList3;
 
+    //past positions of the player used to guide the fish 
     public List<Vector3> targetPositions;
     public List<Vector3> targetPositions2;
     public List<Vector3> targetPositions3;
 
+    //Recorded speeds of the player for each of their past positions
     public List<float> speeds;
     public List<float> speeds2;
     public List<float> speeds3;
@@ -37,7 +40,7 @@ public class FishManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //pastpositons gotten from player object
         targetPositions = targetObject.GetComponent<PlayerController>().pastPositionList;
         targetPositions2 = targetObject.GetComponent<PlayerController>().pastPositionList2;
         targetPositions3 = targetObject.GetComponent<PlayerController>().pastPositionList3;
@@ -45,6 +48,8 @@ public class FishManager : MonoBehaviour
         speeds = player.speedList;
         speeds2 = player.speedList2;
         speeds3 = player.speedList3;
+
+        //iterate through all the fish in each list to move them 
         int i = 0;
         foreach (FishController fish in FishList)
         {
@@ -85,9 +90,11 @@ public class FishManager : MonoBehaviour
             i++;
         }
 
+        //calculate current number of fish the player has
         player.foundFish = FishList.Count + FishList2.Count + FishList3.Count;
     }
 
+    //Spawn fish out of crates and assign them to 1 of three possible lists of fish
     int fishInsertTick = 0;
     public void SpawnFish(int fishToSpawn, Vector3 position)
     {
@@ -110,6 +117,7 @@ public class FishManager : MonoBehaviour
         }
     }
 
+    //Assign fish to 1 of 3 lists when the player touches them
     public void AddToList(FishController fish)
     {
         fish.GetComponent<FishController>().fishSpeed = targetObject.gameObject.GetComponent<PlayerController>().playerSpeed;
@@ -136,6 +144,8 @@ public class FishManager : MonoBehaviour
         fishInsertTick++;
         if (fishInsertTick > 2) fishInsertTick = 0;
     }
+
+    //Remove fish in the front of the queue when colliding with mines. Putting them into a dead state
     int fishRemoveTick = 0;
     public void RemoveFromList()
     {
@@ -167,6 +177,7 @@ public class FishManager : MonoBehaviour
         Debug.Log("FishremoveTick" + fishRemoveTick);
     }
 
+    // Remove fish that contact the eels mouth. Must be removed from the list before being destroyed to avoid errors
     public void EatFish(int fishNumber, int fishTeam, FishController fish   )
     {
         if (fishTeam == 1)
